0273e984d64a1decbdad359d47b05df74c05044a
Shaifjohn <83347476+Shaifjohn@users.noreply.github.com>
2021-05-17 08:51:37 +0200

    InleidingJava

--- src/opdracht116.java
+++ src/opdracht116.java
@@ -1,28 +1,19 @@
-
 import java.awt.*;
 import java.applet.*;
 
 
 public class opdracht116 extends Applet {
-    int a = 0;
-    int b = 1;
-    int c;
+
 
     public void init() {}
 
     public void paint(Graphics g) {
-        int y = 0;
-        y += 20;
-        g.drawString("" + a, 305, y);
-        y += 20;
-        g.drawString("" + b, 305, y);
-
-        for(int teller = 0; teller < 10; teller++) {
-            y += 20;
-            c = a + b;
-            a = b;
-            b = c;
-            g.drawString("" + c, 305, y );
+        int afmeting = 10;
+        for(int teller = 0; teller < 6; teller++) {
+            afmeting += 10;
+            g.drawOval(50, 50, afmeting, afmeting);
         }
+
     }
+
 }
