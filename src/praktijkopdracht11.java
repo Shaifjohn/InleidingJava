62926f2dc04d68cbeabf9a9290358783c8a573ec
Shaifjohn <83347476+Shaifjohn@users.noreply.github.com>
2021-05-19 17:51:18 +0200

    InleidingJava

---
+++ src/praktijkopdracht11.java
@@ -0,0 +1,151 @@
+import java.awt.*;
+import java.applet.*;
+
+
+public class praktijkopdracht11 extends Applet {
+    int breedte;
+    int hoogte;
+    int y;
+    int x;
+
+    ;
+
+    public void init() {
+        breedte = 25;
+        hoogte = 25;
+
+
+    }
+
+
+    public void paint(Graphics g) {
+        y = 50;
+        x = 50;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+
+
+        }
+        y = 75;
+        x = 50;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+
+
+        }
+
+        y = 100;
+        x = 50;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+        y = 125;
+        x = 50;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+
+        }
+
+        y = 150;
+        x = 50;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+
+        }
+        y = 175;
+        x = 50;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+
+        }
+
+        y = 200;
+        x = 50;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+
+
+        }
+
+        y = 225;
+        x = 50;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+
+        }
+
+    }
+
+}
