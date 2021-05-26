0c9ea28cdd4631895e3ad93db9b7cce4e6f7ef08
Shaifjohn <83347476+Shaifjohn@users.noreply.github.com>
2021-05-26 12:15:18 +0200

    InleidingJava

--- src/opdracht121.java
+++ src/opdracht121.java
@@ -3,11 +3,12 @@ import java.applet.*;
 
 public class opdracht121 extends Applet {
     double salaris[];
+    double totaal;
     double gemiddelde;
 
     public void init() {
         salaris = new double [10];
-
+        totaal = 0;
 
         for (int teller = 0; teller < salaris.length; teller ++) {
             salaris[teller] = 100 * teller + 100;
@@ -17,7 +18,11 @@ public class opdracht121 extends Applet {
     public void paint(Graphics g) {
         for (int teller = 0; teller < salaris.length; teller ++) {
             g.drawString("" + salaris[teller], 50, 20 * teller + 20);
+            totaal += salaris[teller];
+
 
         }
+        gemiddelde = totaal / salaris.length;
+        g.drawString("het gemiddelde is" + gemiddelde, 100, 70);
     }
 }
